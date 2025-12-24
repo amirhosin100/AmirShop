22aa5a9327d99cacfe35b7f39629d9b8a91ef27d
amirhossein <computer.super111@gmail.com>
2025-12-24 17:23:51 +0330

    change config folder to "config"

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'AmirShop.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'AmirShop.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
