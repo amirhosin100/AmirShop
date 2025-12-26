292e3dc8a596e77620404028b9e43bfe81a147bb
amirhossein <computer.super111@gmail.com>
2025-12-26 19:03:14 +0330

    created MarketRequestAdmin

--- apps/market_request/admin.py
+++ apps/market_request/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from apps.market_request.models import MarketRequest
+from base.base_admin import BaseAdmin
 
 # Register your models here.
+
+@admin.register(MarketRequest)
+class MarketRequestAdmin(BaseAdmin):
+    list_display = ("user","mobile_number","city","province")
+    search_fields = ("mobile_number","city","province")
+    list_filter = ("city","province")
