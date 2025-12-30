c99919532dfe84f9f8201144befee31c37de038f
amirhossein <computer.super111@gmail.com>
2025-12-30 10:29:18 +0330

    first celery configs

---
+++ config/celery_conf.py
@@ -0,0 +1,10 @@
+from celery import Celery
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+celery_app = Celery('AmirShop')
+
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+
+celery_app.autodiscover_tasks()
